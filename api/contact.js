3678e795a20877425fc8e8354558af1da1385275
Antonio Hunt <hunttony468@gmail.com>
2025-04-23 19:24:38 -0500

    vercel config update 9

--- api/contact.js
+++ api/contact.js
@@ -3,9 +3,7 @@ import connectDB from '../utils/db.js';
   import validator from 'validator';
 
   export default async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Origin, X-Requested-With, Accept');
+    
 
     if (req.method === 'OPTIONS') {
       return res.status(200).end();
--- api/health.js
+++ api/health.js
@@ -1,7 +1,5 @@
 module.exports = async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Origin, X-Requested-With, Accept');
+    
   
     if (req.method === 'OPTIONS') {
       return res.status(200).end();
